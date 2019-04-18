6922457c733448e49ec4e1fc08751c4fe2eb8c56
Burak Kocak <burakkocak884@ghotmail.com>
2019-04-18 12:14:32 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -6,11 +7,33 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+
   end
+
+  def activate
+    @student = Student.find_by(params[:id])
+   
+    if @student.active == false
+      @student.update(active: true)
+    else
+      @student.update(active: false)
+    end
+    @student.save
+    redirect_to student_path
+  end
+
 
   private
 
     def set_student
       @student = Student.find(params[:id])
+
     end
+
+
+
+
+
+
 end
---
+++ db/migrate/002_add_column_students.rb
@@ -0,0 +1,6 @@
+class AddColumnStudents < ActiveRecord::Migration
+  def change
+   add_column :students, :active, :boolean, :default => false
+    end
+ 
+end
